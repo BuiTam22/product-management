fa4d178c6733a072361edcbf101af88a2a7d51e0
Tam <Tam@TAM-DESKTOP>
2023-10-02 11:05:32 +0700

    tao thanh phan trang

--- controllers/admin/products.controllers.js
+++ controllers/admin/products.controllers.js
@@ -1,7 +1,7 @@
 const Product = require('../../models/product.model.js');
 const filterStatusHelpers = require("../../helpers/filterStatus.js");
 const searchHelper = require("../../helpers/search.js");
-
+const paginationHelper = require("../../helpers/pagination");
 module.exports.products = async (req, res) =>{
 
     const filterStatus = filterStatusHelpers(req.query);
@@ -16,13 +16,26 @@ module.exports.products = async (req, res) =>{
     if(req.query.keyword){
       find.title = objectSearch.regex;
     }
-    
+
+    // Pagination
+    let initPagination = {
+        currentPage: 1,
+        limitItems: 4
+    };
+
+    const countProducts = await Product.count(find);
+    const objectPagination = paginationHelper(initPagination, req.query, countProducts);
+
     const products = await Product.find(find)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
+
     res.render('admin/pages/products/index.pug',{
         title: "Danh sách sản phẩm",
         products: products,
         filterStatus : filterStatus,
-        keyword : objectSearch.keyword
+        keyword : objectSearch.keyword,
+        pagination: objectPagination
     })
 
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -42,3 +42,20 @@ if(formSearch){
   });
 }
 // End form search
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if(buttonsPagination.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsPagination.forEach(button => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+
+      url.searchParams.set("page", page);
+
+      window.location.href = url.href;
+    });
+  });
+}
+// End Pagination
