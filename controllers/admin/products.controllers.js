a476aabe2b24f07146e5dbc99dbebecace5f13d9
Tam <Tam@TAM-DESKTOP>
2023-10-15 21:51:01 +0700

    update xóa mềm lần 1

--- controllers/admin/products.controllers.js
+++ controllers/admin/products.controllers.js
@@ -72,9 +72,11 @@ module.exports.changeMulti = async (req, res) => {
 //[DELETE] /admin/products/delelte/:id
 module.exports.deleteItem = async (req, res) =>{
     const id = req.params.id;
-    await Product.updateOne({_id: id}, {deleted: true});
+    await Product.updateOne({_id: id},{
+        deleted: true,
+        deletedAt: new Date()});
     res.redirect("back");
-}
+}   
 
 
 
