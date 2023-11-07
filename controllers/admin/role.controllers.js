8155a72f7dfabc0d14460014fe272aba61224ce4
Tam <Tam@TAM-DESKTOP>
2023-11-07 16:33:04 +0700

    Cập nhật nhóm quyền

--- controllers/admin/role.controllers.js
+++ controllers/admin/role.controllers.js
@@ -31,3 +31,35 @@ module.exports.createPost = async (req, res) =>{
     req.flash("success", `Thêm mới thành công!`);
     res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
+
+
+
+// [GET] /admin/roles/edit/:id 
+module.exports.edit = async (req, res) =>{ 
+    try {
+        const id = req.params.id;
+
+        const data = await Role.findOne({_id:id},{deleted:false});
+
+        res.render('admin/pages/roles/edit.pug',{
+            title: "Chỉnh sửa nhóm quyền", 
+            data:data
+        });
+
+    } catch (error) {
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+    }
+    
+}
+
+
+
+// [PATCH] /admin/roles/edit/:id 
+module.exports.editPatch = async (req, res) =>{ 
+    const id = req.params.id;
+    await Role.updateOne({_id: id}, req.body)
+
+    req.flash("success", `Cập nhật thành công!`);
+    res.redirect(`back`);
+    
+}
