da4e04fc2efe0c25ac2bfe22bfeed3df62f8c3c3
Tam <Tam@TAM-DESKTOP>
2024-02-16 17:05:26 +0700

    Làm trang danh mục bài viết lần 2

--- controllers/admin/posts-category.controllers.js
+++ controllers/admin/posts-category.controllers.js
@@ -1,5 +1,12 @@
+const PostCategorySchema = require("../../models/post-category.model");
 module.exports.index = async (req, res) =>{
+    const records = await PostCategorySchema.find({
+        deleted: false,
+        status: "active"
+    });
+
     res.render('admin/pages/posts-category/index',{
-        title: "Danh mục bài viết"
+        title: "Danh mục bài viết",
+        records: records
     });
 }
