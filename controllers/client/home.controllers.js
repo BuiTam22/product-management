2bb0c7ccc7558da2df7645037566b726776d67b9
Tam <Tam@TAM-DESKTOP>
2024-04-18 16:43:05 +0700

    thêm toàn bộ CSS và thêm 10 limit sản phẩm nổi bật

--- controllers/client/home.controllers.js
+++ controllers/client/home.controllers.js
@@ -3,9 +3,9 @@ const ProductHelper = require("../../helpers/product");
 module.exports.index = async (req, res) => { 
     const productFeatured = await Product.find(
         {deleted: false, featured: "1", status: "active"}
-    );
+    ).limit(10);
     const newProductFeatured = ProductHelper.priceNewProducts(productFeatured);
-    console.log(newProductFeatured)
+    
     res.render("client/pages/home/index.pug", {
         title: "Trang chủ",
         productFeatured: newProductFeatured
