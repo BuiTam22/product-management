9960850d69073e43ae662b5c4e243682fc930bca
Bùi Văn Tâm <tamse22032004@gmail.com>
2024-07-04 16:14:58 +0700

    fix lại private route /chat

--- controllers/client/chat.controllers.js
+++ controllers/client/chat.controllers.js
@@ -1,13 +1,10 @@
 module.exports.index = async (req, res) => {
-    if (req.cookies.token_guest) {
-        _io.on('connection', (socket) => {
-            console.log("Có 1 user kết nối", socket.id);
-        })
-        res.render("client/pages/chat/index.pug", {
-            pageTitle: "Chat"
-        })
-    }else{
-        res.redirect("/auth/login/noUrl");
-    }
- 
+    _io.on('connection', (socket) => {
+        console.log("Có 1 user kết nối", socket.id);
+    })
+    res.render("client/pages/chat/index.pug", {
+        pageTitle: "Chat"
+    })
+
+
 }
--- middlewares/client/auth.middleware.js
+++ middlewares/client/auth.middleware.js
@@ -21,7 +21,36 @@ module.exports.requireAuth = async (req, res, next) => {
     });
 
     if (!user) {
-        res.redirect(`/auth/login`);
+        res.redirect(`/auth/login/haveUrl`);
+        return;
+    }
+
+    const role = await Role.findOne({
+        _id: user.role_id
+    }).select("title permissions");
+
+    // -các router chạy qua middleware này thì sẽ nhận được data này 
+    // -file pug thì chỉ cần dùng user or role ở res.locals với controller thì phải dùng
+    // cả res.locals.user
+    res.locals.user = user;
+    res.locals.role = role;
+
+    next();
+}
+
+module.exports.requireAuthNoUrl = async (req, res, next) => {
+
+    if (!req.cookies.token_guest) {
+        res.redirect(`/auth/login/noUrl`);
+        return;
+    }
+
+    const user = await Account.findOne({
+        token: req.cookies.token_guest
+    });
+
+    if (!user) {
+        res.redirect(`/auth/login/noUrl`);
         return;
     }
 
