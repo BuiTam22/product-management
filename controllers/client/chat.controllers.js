a0958f70ce365e806e244e72d78d302036cff939
Bùi Văn Tâm <tamse22032004@gmail.com>
2024-07-06 21:24:22 +0700

    Làm tính năng chat cơ bản

--- controllers/client/chat.controllers.js
+++ controllers/client/chat.controllers.js
@@ -1,10 +1,33 @@
+const Chat = require("../../models/chats.model");
+const Account = require("../../models/accounts.model");
+
 module.exports.index = async (req, res) => {
-    _io.on('connection', (socket) => {
-        console.log("Có 1 user kết nối", socket.id);
+    const userId = res.locals.user.id;
+
+    //_io.once là chỉ lắng nghe một lần rồi thôi (tránh sau khi load lại trang vẫn còn lắng nghe)
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) =>{
+            const newChat = new Chat({
+                user_id: userId,
+                content:content
+            });
+            await newChat.save();
+        })
     })
+
+    const chats = await Chat.find({
+        deleted: false
+    }).lean();
+
+    for(const chat of chats){
+        const infoUser = await Account.findOne({
+            _id: chat.user_id
+        }).select("fullName")
+        chat.infoUser = infoUser;
+    }
+
     res.render("client/pages/chat/index.pug", {
-        pageTitle: "Chat"
+        pageTitle: "Chat",
+        chats: chats
     })
-
-
 }
